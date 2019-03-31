ae137783efd6f0aaa43bdcdbb4139f7de8bf78a4
AhmetKnKc <46850384+AhmetKnKc@users.noreply.github.com>
2019-03-31 22:17:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.TOKEN = (process.env.BOT_TOKEN); // Your token here
 
 exports.PREFIX = 'e!'; // Your preferred prefix here
 
-exports.GOOGLE_API_KEY = 'AIzaSyBODgau9YjEp2ER_PO0aaV1Pkm3gzmOZLg'; // Your youtube/google API key here
+exports.GOOGLE_API_KEY = 'AIzaSyD44BTukpj9zjntmN9Os8th9IaZXmR_c0Y'; // Your youtube/google API key here
