b07a6d7f536025f66fbb443b77fa18ec96a0162c
AhmetKnKc <46850384+AhmetKnKc@users.noreply.github.com>
2019-04-07 00:30:33 +0300

    Update herkesedm.js

--- komutlar/herkesedm.js
+++ komutlar/herkesedm.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const loglar = require('../loglar.json');
 
 exports.run = (client, message, args) => {
+  if(message.author.id == "452871151794782209" || message.author.id == "295503660903825408")
+  {  
   let mesaj = args.slice(0).join(' ');
 if (mesaj.length < 1) return message.channel.send('Özel DM den göndermek İstediğiniz Mesajı Yazınız.');
   message.delete();
@@ -11,14 +13,14 @@ if (mesaj.length < 1) return message.channel.send('Özel DM den göndermek İste
 
       client.users.forEach(u => {
 u.sendEmbed(mesajat)
-})
-};
+})}};
+
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
   aliases: ['dm'],
-  permLevel: 4
+  permLevel: 0
 };
 
 exports.help = {
