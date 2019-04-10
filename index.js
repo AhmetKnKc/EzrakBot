08e9fd1d04cee363a721a2fbda5bb789ce194137
AhmetKnKc <46850384+AhmetKnKc@users.noreply.github.com>
2019-04-10 21:42:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -228,73 +228,16 @@ client.login(TOKEN);
 
 
 
-client.on("guildMemberAdd", async member => {
-        let sayac = JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
-  let otorole =  JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
-      let arole = otorole[member.guild.id].sayi
-  let giriscikis = JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
-  let embed = new Discord.RichEmbed()
-    .setTitle('Otorol Sistemi')
-    .setDescription(`:loudspeaker: :inbox_tray:  @${member.user.tag}'a Otorol Verildi `)
-.setColor("GREEN")
-    .setFooter("Harmony ", client.user.avatarURL);
-
-  if (!giriscikis[member.guild.id].kanal) {
-    return;
-  }
-
-  try {
-    let giriscikiskanalID = giriscikis[member.guild.id].kanal;
-    let giriscikiskanali = client.guilds.get(member.guild.id).channels.get(giriscikiskanalID);
-    giriscikiskanali.send(`:loudspeaker: :white_check_mark: Hoşgeldin **${member.user.tag}** Rolün Başarıyla Verildi.`);
-  } catch (e) { // eğer hata olursa bu hatayı öğrenmek için hatayı konsola gönderelim.
-    return console.log(e)
-  }
-
-});
-
-client.on("guildMemberAdd", async (member) => {
-      let autorole =  JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
-      let role = autorole[member.guild.id].sayi
-
-      member.addRole(role)
-});
-
-
-
-
-client.on("guildMemberAdd", async member => {
-        let sayac = JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
-  let otorole =  JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
-      let arole = otorole[member.guild.id].sayi
-  let giriscikis = JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
-  let embed = new Discord.RichEmbed()
-    .setTitle('Otorol Sistemi')
-    .setDescription(`:loudspeaker: :inbox_tray:  @${member.user.tag}'a Otorol Verildi `)
-.setColor("GREEN")
-    .setFooter("Harmony ", client.user.avatarURL);
-
-  if (!giriscikis[member.guild.id].kanal) {
-    return;
-  }
-
-  try {
-    let giriscikiskanalID = giriscikis[member.guild.id].kanal;
-    let giriscikiskanali = client.guilds.get(member.guild.id).channels.get(giriscikiskanalID);
-    giriscikiskanali.send(`:loudspeaker: :white_check_mark: Hoşgeldin **${member.user.tag}** Rolün Başarıyla Verildi.`);
-  } catch (e) { // eğer hata olursa bu hatayı öğrenmek için hatayı konsola gönderelim.
-    return console.log(e)
-  }
-
-});
-
-client.on("guildMemberAdd", async (member) => {
-      let autorole =  JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
-      let role = autorole[member.guild.id].sayi
-
-      member.addRole(role)
-});
-
+bot.on('guildMemberAdd', member => {
+	bot.on('message', msg =>{
+			var embed = new Discord.RichEmbed();
+			embed.setColor("GREEN");
+			embed.addField("­","Ezrak Fun Jailbreak sunucusu");
+			embed.setImage(member.user.displayAvatarURL);
+			embed.addField('**HoşGeldin.! __ ' + member.displayName + '__.\nSunucu kuralların gözden geçrimeyi unutma.\n`Sunucu Katılanları` Rolün başarıyla verildi.**', "­");
+			bot.channels.get("559043127109746724").send(embed);
+			let rol = client.guilds.get(message.guild.id).roles.find('name', '`Sunucu Katılanları`');
+			member.addRole(rol);});});
 
 
 
